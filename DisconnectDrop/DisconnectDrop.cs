1c52496625fd455b311b91c98572410f17ce1f39
Spooky <averagewizard13@gmail.com>
2018-12-11 15:44:01 -0700

    Convert spaces -> tabs

--- DisconnectDrop/DisconnectDrop.cs
+++ DisconnectDrop/DisconnectDrop.cs
@@ -1,4 +1,4 @@
-﻿using Smod2;
+using Smod2;
 using Smod2.Attributes;
 using Smod2.EventHandlers;
 using Smod2.Events;
@@ -6,34 +6,34 @@ using Smod2.Config;
 
 namespace DisconnectDrop
 {
-    [PluginDetails(
-        author = "Spooky",
-        name = "DisconnectDrop",
-        description = "Drops player items on disconnection.",
-        id = "xyz.wizardlywonders.DisconnectDrop",
-        version = "1.3",
-        SmodMajor = 3,
-        SmodMinor = 1,
-        SmodRevision = 22
-    )]
-    class DisconnectDrop : Plugin
-    {
-        public override void OnDisable()
-        {
-        }
+	[PluginDetails(
+		author = "Spooky",
+		name = "DisconnectDrop",
+		description = "Drops player items on disconnection.",
+		id = "xyz.wizardlywonders.DisconnectDrop",
+		version = "1.3",
+		SmodMajor = 3,
+		SmodMinor = 1,
+		SmodRevision = 22
+	)]
+	class DisconnectDrop : Plugin
+	{
+		public override void OnDisable()
+		{
+		}
 
-        public override void OnEnable()
-        {
-            this.Info("DisconnectDrop has loaded successfully.");
-        }
+		public override void OnEnable()
+		{
+			this.Info("DisconnectDrop has loaded successfully.");
+		}
 
-        public override void Register()
-        {
-            // Register config settings
-            this.AddConfig(new ConfigSetting("ddrop_inventory_refreshrate", 2, SettingType.NUMERIC, true, "How often player inventories are cached (in seconds)."));
+		public override void Register()
+		{
+			// Register config settings
+			this.AddConfig(new ConfigSetting("ddrop_inventory_refreshrate", 2, SettingType.NUMERIC, true, "How often player inventories are cached (in seconds)."));
 
-            // Register events
-            this.AddEventHandlers(new MiscEventHandler(this), Priority.Highest);
-        }
-    }
+			// Register events
+			this.AddEventHandlers(new MiscEventHandler(this), Priority.Highest);
+		}
+	}
 }
--- DisconnectDrop/MiscEventHandler.cs
+++ DisconnectDrop/MiscEventHandler.cs
@@ -10,115 +10,116 @@ using System.Linq;
 
 namespace DisconnectDrop
 {
-    class MiscEventHandler : IEventHandlerPlayerJoin, IEventHandlerDisconnect, IEventHandlerRoundRestart, IEventHandlerFixedUpdate
-    {
-        private readonly DisconnectDrop plugin;
+	class MiscEventHandler : IEventHandlerPlayerJoin, IEventHandlerDisconnect, IEventHandlerRoundRestart, IEventHandlerFixedUpdate
+	{
+		private readonly DisconnectDrop plugin;
 
-        private float pTime = 0;
-        public Dictionary<string, List<Item>> inventories = new Dictionary<string, List<Item>>(); // steamId: inventory
-        public Dictionary<string, Vector> locations = new Dictionary<string, Vector>();          // steamId: x, y, z
+		private float pTime = 0;
+		public Dictionary<string, List<Item>> inventories = new Dictionary<string, List<Item>>(); // steamId: inventory
+		public Dictionary<string, Vector> locations = new Dictionary<string, Vector>();          // steamId: x, y, z
 
-        public MiscEventHandler(DisconnectDrop plugin) => this.plugin = plugin;
+		public MiscEventHandler(DisconnectDrop plugin) => this.plugin = plugin;
 
-        public void OnPlayerJoin(PlayerJoinEvent ev)
-        {
-            inventories[ev.Player.SteamId] = new List<Item>();
-            locations[ev.Player.SteamId] = Vector.Zero;
-        }
+		public void OnPlayerJoin(PlayerJoinEvent ev)
+		{
+			inventories[ev.Player.SteamId] = new List<Item>();
+			locations[ev.Player.SteamId] = Vector.Zero;
+		}
 
-        public void OnDisconnect(DisconnectEvent ev)
-        {
-            Thread myThread = new Thread(new ThreadStart(RealDisconnectHandler));
-            myThread.Start();
-        }
+		public void OnDisconnect(DisconnectEvent ev)
+		{
+			Thread myThread = new Thread(new ThreadStart(RealDisconnectHandler));
+			myThread.Start();
+		}
 
-        private void RealDisconnectHandler()
-        {
-            // bro this is so stupid. equivalent of setting a 500 ms timeout to read a response from a web request
-            // RIP functional OnDisconnect handler 2018-2018
-            Thread.Sleep(500);
+		private void RealDisconnectHandler()
+		{
+			// bro this is so stupid. equivalent of setting a 500 ms timeout to read a response from a web request
+			// RIP functional OnDisconnect handler 2018-2018
+			Thread.Sleep(500);
 
-            foreach (var inv in inventories)
-            {
-                bool hasfound = false;
-                foreach (var player in plugin.Server.GetPlayers())
-                {
-                    if (player.SteamId == inv.Key)
-                    {
-                        hasfound = true;
-                        break;
-                    }
-                }
-                if (!hasfound)
-                {
-                    // Drop player's cached inventory
-                    foreach (var item in inv.Value)
-                    {
-                        var loc = locations[inv.Key];
+			foreach (var inv in inventories)
+			{
+				bool hasfound = false;
+				foreach (var player in plugin.Server.GetPlayers())
+				{
+					if (player.SteamId == inv.Key)
+					{
+						hasfound = true;
+						break;
+					}
+				}
+				if (!hasfound)
+				{
+					// Drop player's cached inventory
+					foreach (var item in inv.Value)
+					{
+						var loc = locations[inv.Key];
 
-                        plugin.Server.Map.SpawnItem(
-                            item.ItemType,
-                            loc,
-                            Vector.Zero
-                        );
-                    }
+						plugin.Server.Map.SpawnItem(
+							item.ItemType,
+							loc,
+							Vector.Zero
+						);
+					}
 
-                    // Remove player entries
-                    inventories.Remove(inv.Key);
-                    locations.Remove(inv.Key);
-                    break;
-                }
-            }
-        }
+					// Remove player entries
+					inventories.Remove(inv.Key);
+					locations.Remove(inv.Key);
+					break;
+				}
+			}
+		}
 
-        public void OnRoundRestart(RoundRestartEvent ev)
-        {
-            this.inventories = new Dictionary<string, List<Item>>();
-            this.locations = new Dictionary<string, Vector>();
-        }
+		public void OnRoundRestart(RoundRestartEvent ev)
+		{
+			this.inventories = new Dictionary<string, List<Item>>();
+			this.locations = new Dictionary<string, Vector>();
+		}
 
-        private int refreshRate = 2;
-        private DateTime refreshCheck = DateTime.Now.AddSeconds(-1);
+		private int refreshRate = 2;
+		private DateTime refreshCheck = DateTime.Now.AddSeconds(-1);
 
-        internal int GetInvRefreshRate()
-        {
-            if (refreshCheck < DateTime.Now)
-            {
-                refreshRate = plugin.GetConfigInt("ddrop_inventory_refreshrate");
-                refreshCheck = DateTime.Now.AddSeconds(20);
-            }
-            return refreshRate;
-        }
+		internal int GetInvRefreshRate()
+		{
+			if (refreshCheck < DateTime.Now)
+			{
+				refreshRate = plugin.GetConfigInt("ddrop_inventory_refreshrate");
+				refreshCheck = DateTime.Now.AddSeconds(20);
+			}
+			return refreshRate;
+		}
 
 
-        public void OnFixedUpdate(FixedUpdateEvent ev)
-        {
-            // Update cached list of all player inventories every ddrop_inventory_refreshrate seconds
-            pTime -= Time.fixedDeltaTime;
-            if (pTime < 0)
-            {
-                pTime = GetInvRefreshRate();
+		public void OnFixedUpdate(FixedUpdateEvent ev)
+		{
+			// Update cached list of all player inventories every ddrop_inventory_refreshrate seconds
+			pTime -= Time.fixedDeltaTime;
+			if (pTime < 0)
+			{
+				pTime = GetInvRefreshRate();
 
-                try
-                {
-                    // Update cached information
-                    var players = 
-                        plugin.Server.GetPlayers()
-                        .Where(p => p.TeamRole.Role != Role.SPECTATOR && p.TeamRole.Role != Role.UNASSIGNED)
-                        .ToList();
-                    for (int i = 0; i < players.Count; i++)
-                    {
-                        var player = players[i];
+				try
+				{
+					// Update cached information
+					var players = plugin.Server
+						.GetPlayers()
+						.Where(p => p.TeamRole.Role != Role.SPECTATOR && p.TeamRole.Role != Role.UNASSIGNED)
+						.ToList();
 
-                        inventories[player.SteamId] = player.GetInventory();
-                        locations[player.SteamId] = player.GetPosition();
-                    };
-                }
-                catch (Exception e)
-                {
-                    plugin.Info(e.StackTrace);
-                }
-            }
-        }
-    }
+					for (int i = 0; i < players.Count; i++)
+					{
+						var player = players[i];
+
+						inventories[player.SteamId] = player.GetInventory();
+						locations[player.SteamId] = player.GetPosition();
+					};
+				}
+				catch (Exception e)
+				{
+					plugin.Info(e.StackTrace);
+				}
+			}
+		}
+	}
 }
